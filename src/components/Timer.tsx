70c35f96f3d684019c35737192d68db48177f12a
Azam Gulov <gulovazam@gmail.com>
2024-05-21 18:53:08 +0500

    Add Timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import FormatTime from '../helpers/FormatTime';
+
+type TimerProps = {
+  setFinalTime: (time: number) => void;
+  isGameOver: boolean;
+};
+
+export default function Timer({ setFinalTime, isGameOver }: TimerProps) {
+  const [timeElapsed, setTimeElapsed] = useState(Date.now);
+  const [isRunning, setIsRunning] = useState(true);
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isGameOver) {
+      setFinalTime(timeElapsed);
+      setIsRunning(false);
+    }
+
+    if (isRunning) {
+      timer = setInterval(() => {
+        setTimeElapsed(Date.now() - timeElapsed);
+      }, 10);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isRunning, isGameOver]);
+
+  return (
+    <div className="pointer-events-none fixed bottom-2 left-0 right-0 flex justify-center rounded-sm">
+      <p className=" bg-pink-100 p-2">{FormatTime(timeElapsed)}</p>
+    </div>
+  );
+}
