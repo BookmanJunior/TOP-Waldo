3eeff1972babedfd4ae2698aff8a1afbfe2b9e5b
Azam Gulov <gulovazam@gmail.com>
2024-05-29 20:21:14 +0500

    Rewrite handleGuess function
    add DropdownOptions component
    change fetch url

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import Dropdown from './Dropdown';
+import DropdownOptions from './DropdownOptions';
 import Timer from './Timer';
 import { Modal } from './Modal';
-import { CharacterData } from '../types/CharacterData';
 import FetchData from './FetchData';
 import { Map as MapProps } from '../types/Map';
 import { useParams } from 'react-router-dom';
+import getUrl from '../helpers/GetUrl';
 
 export default function Map() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
@@ -13,9 +14,8 @@ export default function Map() {
   const [dropdownPosition, setDropdownPosition] = useState({ x: 0, y: 0 });
   const [finalTime, setFinalTime] = useState(0);
   const { map_id } = useParams();
-  const { state, data, error, setData } = FetchData<MapProps>(
-    `http://localhost:3000/maps/${map_id}`
-  );
+  const url = getUrl();
+  const { state, data, error, setData } = FetchData<MapProps>(`${url}/maps/${map_id}`);
 
   if (state === 'loading') return <div>Loading...</div>;
 
@@ -30,12 +30,9 @@ export default function Map() {
         onClick={handleMapClick}
         src={data.img}></img>
       {dropdown && (
-        <Dropdown
-          setDropdown={setDropdown}
-          dropdownPosition={dropdownPosition}
-          data={data.map_data}
-          handleGuess={handleGuess}
-        />
+        <Dropdown setDropdown={setDropdown} dropdownPosition={dropdownPosition}>
+          <DropdownOptions data={data.map_data} handleGuess={handleGuess} />
+        </Dropdown>
       )}
       <Modal isOpen={isGameOver} finalTime={finalTime} map_id={data.id} />
       <Timer setFinalTime={setFinalTime} isGameOver={isGameOver} />
@@ -59,15 +56,28 @@ export default function Map() {
     }
   }
 
-  function handleGuess(char: CharacterData) {
-    if (Math.abs(char.x - position.x) <= 5 && Math.abs(char.y - position.y) <= 5) {
+  async function handleGuess(e: React.FormEvent, charName: string) {
+    e.preventDefault();
+    setDropdown(false);
+
+    try {
+      const res = await fetch(`${url}/marker/verify`, {
+        mode: 'cors',
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ x: position.x, y: position.y, charName })
+      });
+
+      if (res.status >= 400) {
+        return;
+      }
+
       if (data) {
-        const filteredData = data.map_data.filter((c) => c.name !== char.name);
+        const filteredData = data.map_data.filter((c) => c.name !== charName);
         setData({ ...data, map_data: filteredData });
       }
-      setDropdown(false);
-    } else {
-      setDropdown(false);
+    } catch (error) {
+      console.log(error);
     }
   }
 }
