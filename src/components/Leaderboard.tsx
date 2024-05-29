ca1409fefc944de3dda0252c4235558a680f1314
Azam Gulov <gulovazam@gmail.com>
2024-05-29 15:17:13 +0500

    Format leaderboard entries time
    Add conditional rendering

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,17 +1,20 @@
+import FormatTime from '../helpers/FormatTime';
 import { LeaderboardEntries } from '../types/LeaderboardEntries';
 
 export function Leaderboard({ leaderboardData }: { leaderboardData: LeaderboardEntries[] }) {
+  if (!leaderboardData.length) return <div>Leaderboard has no entries</div>;
+
   return (
     <table className="my-2 w-full text-center">
       <caption className=" bg-black py-1 font-semibold tracking-widest text-white">
         Leaderboard
       </caption>
       <tbody>
-        {leaderboardData.map((p, i) => (
+        {leaderboardData.map((entry, i) => (
           <tr key={i} className="border-b [&>*]:py-1">
             <th scope="row">{i + 1}</th>
-            <td>{p?.name}</td>
-            <td>{p?.time}</td>
+            <td>{entry.name}</td>
+            <td>{FormatTime(entry.time)}</td>
           </tr>
         ))}
       </tbody>
