53bfa59dbff7b22a09e3b31697fc5142a76f57f3
Azam Gulov <gulovazam@gmail.com>
2024-05-29 19:53:43 +0500

    Remove data and handleGuess props
    add children props
    move DropdownOptions to a separate file

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,24 +1,12 @@
-import { useState } from 'react';
-import { CharacterData } from '../types/CharacterData';
-
 type Position = { x: number; y: number };
-
-type DropdownOptionsProps = {
-  data: CharacterData[];
-  handleGuess: (arg0: CharacterData) => void;
-};
 
 type DropdownProps = {
   setDropdown: (dropdown: boolean) => void;
   dropdownPosition: Position;
-} & DropdownOptionsProps;
+  children: React.ReactNode;
+};
 
-export default function Dropdown({
-  setDropdown,
-  dropdownPosition,
-  data,
-  handleGuess
-}: DropdownProps) {
+export default function Dropdown({ setDropdown, dropdownPosition, children }: DropdownProps) {
   return (
     <div
       className="absolute flex translate-x-[-20px] translate-y-[-20px]  flex-wrap gap-4"
@@ -32,41 +20,7 @@ export default function Dropdown({
           setDropdown(false);
         }}
         className="h-[50px] w-[50px] cursor-pointer rounded-[50%] bg-red-800 bg-opacity-30 outline-dashed outline-4 outline-red-400"></div>
-      <DropdownOptions data={data} handleGuess={handleGuess} />
-    </div>
-  );
-}
-
-function DropdownOptions({ data, handleGuess }: DropdownOptionsProps) {
-  const [optionsOpen, setOptionsOpen] = useState(false);
-
-  return (
-    <div>
-      <button
-        onClick={() => setOptionsOpen(!optionsOpen)}
-        className={` mb-2 flex items-center gap-4 rounded-sm bg-white px-4 py-1 after:inline-block 
-        ${optionsOpen ? 'after:rotate-45' : 'after:rotate-[-135deg]'} after:border-b-[2px] after:border-r-[2px]
-         after:border-black after:p-[3px] after:transition-[transform]`}>
-        Who is it?
-      </button>
-      {optionsOpen && (
-        <ul className="rounded-[2px] bg-white">
-          {data.map((char, i) => (
-            <li key={i} className="hover:bg-gray-400">
-              <button
-                onClick={() => handleGuess(char)}
-                className="flex w-full flex-wrap items-center gap-1 px-4 py-1 text-start">
-                {char.name}
-                <img
-                  src={char.img}
-                  alt={char.name}
-                  className="-order-last aspect-square w-[75px] object-cover"
-                />
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
+      {children}
     </div>
   );
 }
