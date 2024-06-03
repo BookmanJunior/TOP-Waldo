2ff1fb070baa8328fafe4165db39a7208c8af6e8
Azam Gulov <gulovazam@gmail.com>
2024-06-03 16:25:03 +0500

    Move isGameOver to a separate useEffect
    add startTime ref
    add derive variable time

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import FormatTime from '../helpers/FormatTime';
 
 type TimerProps = {
@@ -7,20 +7,29 @@ type TimerProps = {
 };
 
 export default function Timer({ setFinalTime, isGameOver }: TimerProps) {
-  const [timeElapsed, setTimeElapsed] = useState(Date.now);
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const startTime = useRef(0);
   const [isRunning, setIsRunning] = useState(true);
+
+  const time = timeElapsed - startTime.current;
+
+  useEffect(() => {
+    if (isGameOver) {
+      setFinalTime(time);
+      setIsRunning(false);
+    } else {
+      startTime.current = Date.now();
+      setTimeElapsed(Date.now());
+      setIsRunning(true);
+    }
+  }, [isGameOver]);
 
   useEffect(() => {
     let timer: number;
 
-    if (isGameOver) {
-      setFinalTime(timeElapsed);
-      setIsRunning(false);
-    }
-
     if (isRunning) {
       timer = setInterval(() => {
-        setTimeElapsed(Date.now() - timeElapsed);
+        setTimeElapsed(Date.now());
       }, 10);
     }
 
@@ -28,11 +37,11 @@ export default function Timer({ setFinalTime, isGameOver }: TimerProps) {
       clearInterval(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isRunning, isGameOver]);
+  }, [isRunning]);
 
   return (
     <div className="pointer-events-none fixed bottom-2 left-0 right-0 flex justify-center">
-      <p className=" rounded-md bg-pink-100 bg-opacity-90 p-2">{FormatTime(timeElapsed)}</p>
+      <p className=" rounded-md bg-pink-100 bg-opacity-90 p-2">{FormatTime(time)}</p>
     </div>
   );
 }
