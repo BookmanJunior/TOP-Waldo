12a5865430a626028d901f22a471762f7861b51a
Azam Gulov <gulovazam@gmail.com>
2024-06-03 20:41:16 +0500

    Fetch data on mount

--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -1,5 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useOutletContext } from 'react-router-dom';
+import FetchData from '../components/FetchData';
+import getUrl from '../helpers/GetUrl';
+import { MapPreviewProps } from '../types/MapPreview';
+
+type ContextProps = {
+  mapData: MapPreviewProps[];
+};
 
 export default function Root() {
-  return <Outlet />;
+  const { state, data, error } = FetchData<MapPreviewProps[]>(`${getUrl()}/maps`);
+
+  if (state === 'loading') return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
+  return <Outlet context={{ mapData: data }} />;
+}
+
+export function useRootData() {
+  return useOutletContext<ContextProps>();
 }
