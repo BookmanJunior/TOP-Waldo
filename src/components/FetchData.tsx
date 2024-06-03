0f3ade151acd3876469d16e1ac6619a50e33e4cd
Azam Gulov <gulovazam@gmail.com>
2024-06-03 13:59:20 +0500

    Move fetch function outside of useEffect
    add refetch property to types
    return refetch

--- src/components/FetchData.tsx
+++ src/components/FetchData.tsx
@@ -5,30 +5,33 @@ type LoadState<T> =
   | { state: 'error'; error: Error; data?: undefined }
   | { state: 'loaded'; data: T; error: undefined };
 
-type FetchDataResult<T> = LoadState<T> & { setData: (data: T) => void };
+type FetchDataResult<T> = LoadState<T> & {
+  setData: (data: T) => void;
+  refetchData: () => void;
+};
 
 export default function FetchData<T>(fetchUrl: string): FetchDataResult<T> {
   const [loadState, setLoadState] = useState<LoadState<T>>({ state: 'loading' });
   const setData = (data: T) => setLoadState({ state: 'loaded', data, error: undefined });
 
-  useEffect(() => {
-    async function getData() {
-      try {
-        const res = await fetch(fetchUrl);
-        const resResult = await res.json();
-        if (res.status >= 400) {
-          throw new Error(resResult.message);
-        }
-        setLoadState({ state: 'loaded', data: resResult, error: undefined });
-      } catch (error) {
-        if (error instanceof Error) {
-          setLoadState({ state: 'error', error });
-        }
+  const fetchData = async () => {
+    try {
+      const res = await fetch(fetchUrl);
+      const resResult = await res.json();
+      if (res.status >= 400) {
+        throw new Error(resResult.message);
+      }
+      setLoadState({ state: 'loaded', data: resResult, error: undefined });
+    } catch (error) {
+      if (error instanceof Error) {
+        setLoadState({ state: 'error', error });
       }
     }
+  };
 
-    getData();
+  useEffect(() => {
+    fetchData();
   }, []);
 
-  return { ...loadState, setData };
+  return { ...loadState, setData, refetchData: fetchData };
 }
