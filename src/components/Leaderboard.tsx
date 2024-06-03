29bb186b57441a81398b0e354d62683bafb4efa4
Azam Gulov <gulovazam@gmail.com>
2024-06-03 18:42:49 +0500

    Add showCaption prop
    add interface
    add thead

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,17 +1,31 @@
 import FormatTime from '../helpers/FormatTime';
 import { LeaderboardEntries } from '../types/LeaderboardEntries';
 
-export function Leaderboard({ leaderboardData }: { leaderboardData: LeaderboardEntries[] }) {
+interface LeaderboardProps {
+  leaderboardData: LeaderboardEntries[];
+  showCaption?: boolean;
+}
+
+export function Leaderboard({ leaderboardData, showCaption = true }: LeaderboardProps) {
   if (!leaderboardData.length) return <div>Leaderboard has no entries</div>;
 
   return (
-    <table className="my-2 w-full text-center">
-      <caption className=" bg-black py-1 font-semibold tracking-widest text-white">
-        Leaderboard
-      </caption>
+    <table className="w-full text-center">
+      {showCaption && (
+        <caption className=" mb-4 bg-black py-1 font-semibold tracking-widest text-white">
+          Leaderboard
+        </caption>
+      )}
+      <thead>
+        <tr>
+          <th>Place</th>
+          <th>Name</th>
+          <th>Time</th>
+        </tr>
+      </thead>
       <tbody>
         {leaderboardData.map((entry, i) => (
-          <tr key={i} className="border-b [&>*]:py-1">
+          <tr key={i} className="border-b last:border-0 [&>*]:py-1">
             <th scope="row">{i + 1}</th>
             <td>{entry.name}</td>
             <td>{FormatTime(entry.time)}</td>
