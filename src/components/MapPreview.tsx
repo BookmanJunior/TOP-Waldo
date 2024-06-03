9f32591c2488aea376d5149dae687f5af788a199
Azam Gulov <gulovazam@gmail.com>
2024-06-03 18:24:21 +0500

    Change z-index

--- src/components/MapPreview.tsx
+++ src/components/MapPreview.tsx
@@ -6,11 +6,11 @@ export default function MapPreview({ img, map_id, title }: MapPreviewProps) {
     <li className="group overflow-hidden rounded-md border-4 border-gray-400">
       <Link className="grid h-[300px]" to={`play/${map_id}`}>
         <img
-          className="z-[-1] col-start-1 row-start-1 h-[300px] w-[100%] object-cover object-top transition-[transform] group-hover:scale-110"
+          className="col-start-1 row-start-1 h-[300px] w-[100%] object-cover object-top transition-[transform] group-hover:scale-110"
           src={img}
         />
         <p
-          className="col-start-1 row-start-1 grid min-h-[50px] w-full translate-y-[100%] place-content-center self-end
+          className="z-[2] col-start-1 row-start-1 grid min-h-[50px] w-full translate-y-[100%] place-content-center self-end
          justify-self-center bg-black bg-opacity-60 text-center text-xl text-white transition-[transform] group-hover:translate-y-0">
           {title}
         </p>
