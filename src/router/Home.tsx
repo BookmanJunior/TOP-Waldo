e24493c7f3a7b4b6fbf6b72a3cd23ab2ab7744c7
Azam Gulov <gulovazam@gmail.com>
2024-06-06 14:50:42 +0500

    Change styling

--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -6,9 +6,11 @@ export default function Home() {
   const { mapData } = useRootData();
 
   return (
-    <main className="mx-auto w-[1000px] py-4">
-      <h1 className="mb-2 text-center text-2xl font-semibold">Maps</h1>
-      <ul className="grid grid-cols-[repeat(auto-fit,minmax(300px,300px))] justify-center gap-4">
+    <main className="mx-auto w-[1000px] px-4">
+      <h1 className="mb-2 border-b-[1px] border-gray-700 border-opacity-50 py-4 pb-1 text-2xl font-semibold">
+        Maps
+      </h1>
+      <ul className="grid grid-cols-3 gap-4 max-[650px]:grid-cols-[repeat(auto-fit,minmax(300px,1fr))]">
         {mapData.map((preview) => (
           <MapPreview
             key={preview.map_id}
