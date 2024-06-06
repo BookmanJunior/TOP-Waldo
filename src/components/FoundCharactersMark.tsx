9805110d1d23d0174174e61cc610cee56eb9af21
Azam Gulov <gulovazam@gmail.com>
2024-06-06 14:41:10 +0500

    Add key

--- src/components/FoundCharactersMark.tsx
+++ src/components/FoundCharactersMark.tsx
@@ -8,6 +8,7 @@ type FoundCharactersMark = {
 export default function FoundCharactersMark({ foundPositions }: FoundCharactersMark) {
   return foundPositions.map((position) => (
     <img
+      key={position.y + position.x}
       style={{ top: `${position.y}%`, left: `${position.x}%` }}
       src={Checkmark}
       className="pointer-events-none absolute w-[24px]"></img>
