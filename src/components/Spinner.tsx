95e0fbac3ed20150c42303c93b77aa630beec50e
Azam Gulov <gulovazam@gmail.com>
2024-06-09 20:04:47 +0500

    Add optional prop className

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,6 @@
-export default function Spinner() {
+export default function Spinner({ className }: { className?: string }) {
   return (
-    <div className=" aspect-square w-[40px] animate-spin self-center rounded-[50%] border-4 border-b-orange-400 border-t-orange-400"></div>
+    <div
+      className={`${className} aspect-square w-[40px] animate-spin self-center rounded-[50%] border-4 border-b-orange-400 border-t-orange-400`}></div>
   );
 }
